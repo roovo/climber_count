2c296147e6969774e961215aa32d84fa45dcf9ab
rupert <rupert.voelcker@bt.com>
2020-07-25 19:48:42 +0100

    ghPages: render graph from csv

---
+++ js/climb_count.js
@@ -0,0 +1,15 @@
+csvUrl = "https://raw.githubusercontent.com/roovo/climber_count/gh-pages/data/climber_count.csv"
+
+d3.csv(csvUrl, function(d) {
+  return {
+    time: new Date(d.time),
+    count: +d.count
+  };
+
+}, function(error, rows) {
+  var chart = d3_timeseries()
+          .addSerie(rows, { x: 'time', y: 'count' }, { interpolate: 'linear', color: "#a6cee3", label: "climbers" })
+          .width(650)
+          .height(300)
+  chart('#chart');
+});
