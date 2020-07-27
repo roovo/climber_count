3af959666f0b9c84c354d3d5da301a6f61033260
roovo <rupert.voelcker@gmail.com>
2020-07-27 21:57:32 +0100

    set count to 0 every hour whilst gym is closed

--- script/scrape_occupancy.rb
+++ script/scrape_occupancy.rb
@@ -9,58 +9,71 @@ require 'nokogiri'
 require 'net/ssh'
 require 'net/scp'
 
-logger          = Logger.new(STDOUT)
-logger.level    = Logger::INFO
+LOGGER          = Logger.new(STDOUT)
+LOGGER.level    = Logger::INFO
 
-@time            = Time.now
+@time           = Time.now
 
 buffer          = 15 * 60             #  seconds
 
-WEEKEND_OPEN   = (Time.parse "10:00") - buffer
-WEEKEND_CLOSE  = (Time.parse "18:00") + buffer
+WEEKEND_OPEN    = (Time.parse "10:00") - buffer
+WEEKEND_CLOSE   = (Time.parse "18:00") + buffer
 
-WEEKDAY_OPEN   = (Time.parse "12:00") - buffer
-WEEKDAY_CLOSE  = (Time.parse "21:30") + buffer
+WEEKDAY_OPEN    = (Time.parse "12:00") - buffer
+WEEKDAY_CLOSE   = (Time.parse "21:30") + buffer
 
-def is_on_the_hour?
-  @time.min.zero?
-end
+DATA_URI        = ENV.fetch("DATA_URI")
+UPLOAD_IP       = ENV.fetch("UPLOAD_IP")
+UPLOAD_PORT     = ENV.fetch("UPLOAD_PORT")
+UPLOAD_PATH     = ENV.fetch("UPLOAD_PATH")
+UPLOAD_USER     = ENV.fetch("UPLOAD_USER")
+KEY_PATH        = ENV.fetch("KEY_PATH")
 
 def during_weekend_opening
   if @time.saturday? || @time.sunday?
     if (@time >= WEEKEND_OPEN) && (@time <= WEEKEND_CLOSE)
       true
     else
-      is_on_the_hour?
+      false
     end
   else
-    is_on_the_hour?
+    false
   end
 end
 
 def during_weekday_opening
   if @time.saturday? || @time.sunday?
-    is_on_the_hour?
+    false
   else
     if (@time >= WEEKDAY_OPEN) && (@time <= WEEKDAY_CLOSE)
       true
     else
-      is_on_the_hour?
+      false
     end
   end
 end
 
+def is_on_the_hour?
+  @time.min.zero?
+end
+
+def write_count(count)
+  LOGGER.info "Updating count: #{count}"
+
+  CSV.open("./data/climber_count.csv", "a") do |csv|
+    csv << [Time.now.utc.to_s.gsub(" UTC", "Z").gsub(" ", "T"), count.to_s]
+  end
+
+  Net::SCP.start(UPLOAD_IP, UPLOAD_USER, port: UPLOAD_PORT, keys: [KEY_PATH]) do |ssh|
+    ssh.upload! "./data/climber_count.csv", UPLOAD_PATH
+  end
+end
+
 if during_weekend_opening || during_weekday_opening
-  data_uri    = ENV.fetch("DATA_URI")
-  upload_ip   = ENV.fetch("UPLOAD_IP")
-  upload_port = ENV.fetch("UPLOAD_PORT")
-  upload_path = ENV.fetch("UPLOAD_PATH")
-  upload_user = ENV.fetch("UPLOAD_USER")
-  key_path    = ENV.fetch("KEY_PATH")
 
-  logger.info "Scrape Starting"
+  LOGGER.info "Scrape Starting"
 
-  html = URI.open data_uri
+  html = URI.open DATA_URI
 
   doc = Nokogiri::HTML(html)
 
@@ -68,19 +81,16 @@ if during_weekend_opening || during_weekday_opening
   occupancy         = JSON.parse(occupancy_string.gsub("'", "\"").gsub(/\s+/, "").gsub("},}","}}"))
   count             = occupancy["AAA"]["count"]
 
-  logger.info "Updating CSV"
+  write_count(count)
 
-  CSV.open("./data/climber_count.csv", "a") do |csv|
-    csv << [Time.now.utc.to_s.gsub(" UTC", "Z").gsub(" ", "T"), count.to_s]
-  end
-
-  logger.info "Uploading CSV"
-
-  Net::SCP.start(upload_ip, upload_user, port: upload_port, keys: [key_path]) do |ssh|
-    ssh.upload! "./data/climber_count.csv", upload_path
-  end
-
-  logger.info "Scrape Complete"
+  LOGGER.info "Scrape Complete"
 else
-  logger.info "No scrape: gym closed"
+  LOGGER.info "No Scrape: Gym Closed"
+
+  if is_on_the_hour?
+    write_count(0)
+  end
+
+  LOGGER.info "Update Complete"
 end
+
