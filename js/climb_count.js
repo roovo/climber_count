b638294bacaf77822bf462d672ce39a6870cda16
roovo <rupert.voelcker@gmail.com>
2020-09-26 16:16:44 +0100

    ghPages: only show previous week of data

--- js/climb_count.js
+++ js/climb_count.js
@@ -1,5 +1,7 @@
 csvUrl = "https://roovoweb.com/data/climber_count.csv"
 
+const DAYS_TO_SHOW = 7;
+
 d3.csv(csvUrl, function(d) {
   return {
     time: new Date(d.time),
@@ -7,8 +9,16 @@ d3.csv(csvUrl, function(d) {
   };
 
 }, function(error, rows) {
+  var startOfWeek = new Date();
+      startOfWeek.setHours(0, 0, 0, 0);
+      startOfWeek.setDate(startOfWeek.getDate() - DAYS_TO_SHOW);
+
+  var filtered = rows.filter(function(r) {
+    return r.time > startOfWeek;
+  });
+
   var chart = d3_timeseries()
-          .addSerie(rows, { x: 'time', y: 'count' }, { interpolate: 'linear', color: "#a6cee3", label: "climbers" })
+          .addSerie(filtered, { x: 'time', y: 'count' }, { interpolate: 'linear', color: "#a6cee3", label: "climbers" })
           .width(650)
           .height(300)
   chart('#chart');
